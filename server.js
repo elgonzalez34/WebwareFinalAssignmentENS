b079327b08e93cdf073f77fb5fa4a97cfec14c35
elgonzalez34 <elgonzalez@wpi.edu>
2016-12-07 23:22:44 -0500

    first commit

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
-var http = require('http')
-  , fs   = require('fs')
-  , url  = require('url')
-  , port = 8080;
+var http = require('http'),
+    fs = require('fs'),
+    url = require('url'),
+    port = 8080;
 
-var server = http.createServer (function (req, res) {
-  var uri = url.parse(req.url)
+var server = http.createServer(function(req, res) {
+    var uri = url.parse(req.url)
 
-  switch( uri.pathname ) {
-    case '/':
-      sendFile(res, 'index.html')
-      break
-    case '/index.html':
-      sendFile(res, 'index.html')
-      break
-    case '/style.css':
-      sendFile(res, 'style.css', 'text/css')
-      break
-    default:
-      res.end('404 not found')
-  }
+    switch (uri.pathname) {
+        case '/':
+            sendFile(res, 'public/index.html', 'text/html')
+            break
+        case '/index.html':
+            sendFile(res, 'public/index.html', 'text/html')
+            break
+        case '/css/style.css':
+            sendFile(res, 'public/css/style.css', 'text/css')
+            break
+        default:
+            res.end('404 not found')
+    }
 })
 
 server.listen(process.env.PORT || port);
@@ -26,11 +26,11 @@ console.log('listening on 8080')
 
 // subroutines
 
-function sendFile(res, filename) {
+function sendFile(res, filename, contentType) {
 
-  fs.readFile(filename, function(error, content) {
-    res.writeHead(200, {'Content-type': 'text/html'})
-    res.end(content, 'utf-8')
-  })
+    fs.readFile(filename, function(error, content) {
+        res.writeHead(200, { 'Content-type': contentType })
+        res.end(content, 'utf-8')
+    })
 
 }
